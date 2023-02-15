0ac7d863547c3de1dd4521d4e485f5ea2d1dca1b
Kacper Rubaszewski <k.rubaszewski96@gmail.com>
2023-02-15 13:49:28 +0100

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Kacper Rubaszewski on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
